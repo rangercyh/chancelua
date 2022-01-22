7fb7e56eafd233b8672d1fe29da9d3ca581e7650
caiyiheng <yiheng.cai@centurygame.com>
2022-01-22 12:00:32 +0800

    rm comment

--- mt/basics_mt.lua
+++ mt/basics_mt.lua
@@ -2,14 +2,6 @@ local helper = require "utils.helper"
 
 local mt = {}
 
---[[
-    Return a random bool, either true or false
-
-    @param {Object} [options={ likelihood: 50 }] alter the likelihood of
-          receiving a true or false value back.
-    @throws {RangeError} if the likelihood is out of bounds
-    @returns {Bool} either true or false
-]]
 function mt:bool(options)
     -- likelihood of success (true)
     options = helper.init_options(options, {likelihood = 50})
@@ -48,13 +40,6 @@ function mt:animal(options)
     return self:pick(self:get("animals")[self:pick(animal_type)])
 end
 
---[[
-    Return a random character.
-
-    @param {Object} [options ={}] can specify a character pool or alpha,
-    numeric, symbols and casing (lower or upper)
-    @returns {String} a single random character
-]]
 function mt:character(options)
     options = helper.init_options(options)
     local symbols = "!@#$%^&*()[]"
@@ -88,20 +73,6 @@ function mt:character(options)
     return pool:sub(index, index)
 end
 
--- Note, wanted to use "float" or "double" but those are both JS reserved words.
-
--- Note, fixed means N OR LESS digits after the decimal. This because
--- It could be 14.9000 but in JavaScript, when this is cast as a number,
--- the trailing zeroes are dropped. Left to the consumer if trailing zeroes are
--- needed
---[[
-    Return a random floating point number
-
-    @param {Object} [options={}] can specify a fixed precision, min, max
-    @returns {Number} a single floating point number
-    @throws {RangeError} Can only specify fixed or precision, not both.
-        Also min cannot be greater than max
-]]
 function mt:floating(options)
     options = helper.init_options(options, {fixed = 4})
     local INT_MAX = math.tointeger(2 ^ 32)
@@ -120,34 +91,12 @@ function mt:floating(options)
     return math.floor(num * 10 ^ options.fixed) / 10 ^ options.fixed
 end
 
---[[
-    Return a random integer
-
-    NOTE the max and min are INCLUDED in the range. So:
-    chance.integer({min: 1, max: 3});
-    would return either 1, 2, or 3.
-
-    @param {Object} [options={}] can specify a min and/or max
-    @returns {Number} a single random integer number
-    @throws {RangeError} min cannot be greater than max
-]]
 function mt:integer(options)
     options = helper.init_options(options, {min = helper.INT_MIN, max = helper.INT_MAX})
     assert(options.min <= options.max, "Chance: Min cannot be greater than Max.")
     return math.floor(self:random() * (options.max - options.min + 1) + options.min)
 end
 
---[[
-    Return a random natural
-
-    NOTE the max and min are INCLUDED in the range. So:
-    chance.natural({min: 1, max: 3});
-    would return either 1, 2, or 3.
-
-    @param {Object} [options={}] can specify a min and/or max or a numerals count.
-    @returns {Number} a single random integer number
-    @throws {RangeError} min cannot be greater than max
-]]
 function mt:natural(options)
     options = helper.init_options(options, {min = 1, max = helper.INT_MAX})
     if type(options.numerals) == "number" then
@@ -174,15 +123,6 @@ function mt:natural(options)
     return self:integer(options)
 end
 
---[[
-*  Return a random prime number
-     *
-     *  NOTE the max and min are INCLUDED in the range.
-     *
-     *  @param {Object} [options={}] can specify a min and/or max
-     *  @returns {Number} a single random prime number
-     *  @throws {RangeError} min cannot be greater than max nor negative
-]]
 function mt:prime(options)
     options = helper.init_options(options, { min = 0, max = 10000 })
     assert(options.min >= 0, "Chance: Min cannot be less than zero.")
@@ -205,17 +145,6 @@ function mt:prime(options)
     return self:pick(target_primes)
 end
 
---[[
-*  Return a random hex number as string
-     *
-     *  NOTE the max and min are INCLUDED in the range. So:
-     *  chance.hex({min: '9', max: 'B'});
-     *  would return either '9', 'A' or 'B'.
-     *
-     *  @param {Object} [options={}] can specify a min and/or max and/or casing
-     *  @returns {String} a single random string hex number
-     *  @throws {RangeError} min cannot be greater than max
-]]
 function mt:hex(options)
     options = helper.init_options(options, { min = 0, max = helper.INT_MAX, casing = "lower" })
     assert(options.min >= 0, "Chance: Min cannot be less than zero.")
@@ -235,13 +164,7 @@ function mt:letter(options)
     end
     return letter
 end
---[[
-*  Return a random string
-     *
-     *  @param {Object} [options={}] can specify a length or min and max
-     *  @returns {String} a string of random length
-     *  @throws {RangeError} length cannot be less than zero
-]]
+
 function mt:string(options)
     options = helper.init_options(options, { min = 5, max = 20 })
     if not(options.length) then
@@ -251,28 +174,6 @@ function mt:string(options)
     return table.concat(self:n(self.character, self, options.length, options))
 end
 
---[[
-*  Return a random string matching the given template.
-     *
-     *  The template consists of any number of "character replacement" and
-     *  "character literal" sequences. A "character replacement" sequence
-     *  starts with a left brace, has any number of special replacement
-     *  characters, and ends with a right brace. A character literal can be any
-     *  character except a brace or a backslash. A literal brace or backslash
-     *  character can be included in the output by escaping with a backslash.
-     *
-     *  The following replacement characters can be used in a replacement
-     *  sequence:
-     *
-     *      "#": a random digit
-     *      "a": a random lower case letter
-     *      "A": a random upper case letter
-     *
-     *  Example: chance.template('{AA###}-{##}')
-     *
-     *  @param {String} template string.
-     *  @returns {String} a random string matching the template.
-]]
 function mt:template(template)
     if not template or string.len(template) == 0 then
         assert(false, "Template string is required")
